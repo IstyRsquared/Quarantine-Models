0cb7a7a77c4d58f279d26914428ff1c686be9e1c
Kristyna Rysava <istyrysava@gmail.com>
2022-11-17 11:24:53 -0500

    add computational part

---
+++ Computational part/SEIRVQ_plot.R
@@ -0,0 +1,152 @@
+## Hong KONG model development
+## Name: Isty Rysava
+## Date: 27/06/18
+## Code: reads in similuation outputs of 3 quarantine scenarios and draws monthly ts of human and dog cases
+
+rm(list=ls())
+setwd("~/Dropbox/HongKong")
+library(harrypotter)
+
+### Data 
+## original
+# # out
+# sc1 <- readRDS("output/out_thesis_scenario1.Rdata")
+# sc2 <- readRDS("output/out_thesis_scenario2..Rdata")
+# sc3 <- readRDS("output/out_thesis_scenario3.Rdata")
+# 
+# # counts (has dog and human deaths weekly)
+# sc1a <- readRDS("output/counts_thesis_scenario1.Rdata")
+# sc2a <- readRDS("output/counts_thesis_scenario2.Rdata")
+# sc3a <- readRDS("output/counts_thesis_scenario3.Rdata")
+
+## 50% vaccination
+# # counts (has dog and human deaths weekly)
+# sc1a <- readRDS("output/counts_thesis_scenario1halfvacc.Rdata")
+# sc2a <- readRDS("output/counts_thesis_scenario2halfvacc.Rdata")
+# sc3a <- readRDS("output/counts_thesis_scenario3halfvacc.Rdata")
+
+## extra 50% vaccination
+# counts (has dog and human deaths weekly)
+sc1a <- readRDS("output/counts_thesis_scenario1extravacc.Rdata")
+sc2a <- readRDS("output/counts_thesis_scenario2extravacc.Rdata")
+sc3a <- readRDS("output/counts_thesis_scenario3extravacc.Rdata")
+
+### Set up a desired simulation to process ##
+sc <- sc3a
+name <- "sc3counts"
+nsim <- length(sc)
+time <- 1:length(sc[[1]][,1])
+treatment <- "extravacc" # original, halfvacc, extravacc
+
+pathA <- paste0("output/", name, "_")
+pathB <- paste0("output/", name, "_monthly_")
+pathC <- paste("output/", name, "_statsmonthly_")
+pathD <- paste0("figs/monthly_ts_", name, "_", treatment, ".pdf")
+
+## Substract populations of interest ##
+# initialize matrices
+deadD <- matrix(NA, ncol=nsim, nrow=length(time))
+deadH <- matrix(NA, ncol=nsim, nrow=length(time))
+
+# populate
+for(i in 1:nsim){
+  colnames(sc[[i]]) <- c("S", "V1", "V2", "V3", "E", "I", "Qs", "Qer", "Qeb", "Qi", "Rill", "Sh", "Eh",
+                         "Ih", "Rh", "Vhs", "Vhe")
+  deadD[,i] <- sc[[i]][,"Rill"]
+  deadH[,i] <- sc[[i]][,"Rh"]
+}
+
+# save
+# write.csv(deadD, paste0(pathA, "deadD.csv"), row.names=F)
+# write.csv(deadH, paste0(pathA, "deadH.csv"), row.names=F)
+
+## Aggregate by month ##
+weeks <- seq(as.Date("2018-01-01"), as.Date("2022-12-31"), by="week")
+months <- ((as.POSIXlt(strptime(weeks, format="%Y-%m-%d"))$year-118)*12) + as.POSIXlt(strptime(weeks, format="%Y-%m-%d"))$mon+1
+
+# initialize lists
+my.files <- list(deadD, deadH)
+sum_mthly <- vector("list", length(my.files))
+names(my.files) <- list("deadD", "deadH")
+names(sum_mthly) <- list("deadD", "deadH")
+
+# aggreagte
+for(i in 1: length(my.files)){
+  df.temp <- data.frame(my.files[[i]])
+  sum_mthly[[i]] <- rowsum(df.temp, group=months)
+}
+
+# save 
+# write.csv(sum_mthly[[4]], paste0(pathB, "deadD.csv"), row.names=F)
+# write.csv(sum_mthly[[5]], paste0(pathB, "deadH.csv"), row.names=F)
+
+### Calculate statistics ###
+# initialize list
+stats_mthly <- vector("list", length(sum_mthly))
+names(stats_mthly) <- list("deadD", "deadH")
+
+for(i in 1:length(sum_mthly)){
+  df.temp <- data.frame(sum_mthly[[i]])
+  monthly_stats <- data.frame(mean=as.numeric(rowMeans(df.temp)))
+  for(j in 1:nrow(monthly_stats)){
+    monthly_stats$upperPI[j] <- as.numeric(as.character(sort(df.temp[j,])[round(0.975*nsim)]))
+    monthly_stats$lowerPI[j] <- as.numeric(as.character(sort(df.temp[j,])[round(0.275*nsim)]))
+  }
+  stats_mthly[[i]] <- monthly_stats
+}
+
+# save 
+# write.csv(stats_mthly[[1]], paste0(pathC, "deadD.csv"), row.names=F)
+# write.csv(stats_mthly[[2]], paste0(pathC, "deadH.csv"), row.names=F)
+
+### Prepare colours
+pal <- hp(n = 10, house = "Slytherin")
+plot(1:10, 1:10, col=pal, pch=16, cex=3)
+colgreen <- pal[10]
+colgreenlight <- pal[3]
+
+pal <- hp(n = 10, option = "LunaLovegood")
+plot(1:10, 1:10, col=pal, pch=16, cex=3)
+colpink <- pal[7]
+colpinkdark <- pal[10]
+
+### Plot time series ###
+# of dead dogs and humans
+dogs <- stats_mthly[[1]]
+colSums(dogs)
+humans <- stats_mthly[[2]]
+colSums(humans)
+
+pdf(pathD, width=6, height=4)
+par(mar=c(5,5.5,1,1.5), cex=0.9)
+# main dogs
+x<-1:nrow(dogs)
+#plot(dogs$mean~x,type="l",cex.lab=1.2,ylab="Canine rabies cases",xlab="Time (months)",axes=F,ylim=c(0,max(dogs$upperPI)), xaxt="n", bty="n")
+plot(dogs$mean~x,type="l",cex.lab=1,ylab="Canine rabies cases",xlab="Time (months)", axes=F,ylim=c(0,130), xaxt="n", bty="n")
+axis(1, at=seq(1,61,12), labels=paste("Jan",c(2018, 2019, 2020, 2021, 2022, 2023)), cex.axis=0.9)
+axis(2, cex.axis=0.9)
+axis(1,at=seq(1,61,3),lab=rep("",length(seq(1,61,3))),tck=-0.01)
+polygon(c(1:nrow(dogs),nrow(dogs):1),c(dogs$upperPI,rev(dogs$lowerPI)),col=colpink,border=F)
+lines(dogs$mean~x,col=colpinkdark,lwd=2)
+
+# inset humans
+par(new=TRUE, mar=c(0,0,0,0),mfrow=c(1,1), plt=c(0.67, 0.93, 0.67, 0.89), cex=0.72)
+x<-1:nrow(humans)
+#plot(humans$mean~x,type="l",cex.lab=1.2,ylab="Human rabies cases",xlab="Time (months)",axes=F,ylim=c(0,max(humans$upperPI)), xaxt="n", bty="n")
+plot(humans$mean~x,type="l",cex.lab=1,ylab="Human rabies cases",xlab="Time (months)",axes=F,ylim=c(0,10), xaxt="n", bty="n")
+axis(1,at=seq(1,61,12),labels=c(2018, 2019, 2020, 2021, 2022, 2023), cex.axis=0.8)
+axis(1, at=seq(1,61,3), lab=rep("",length(seq(1,61,3))),tck=-0.01)
+axis(2, cex.axis=.8)
+polygon(c(1:nrow(humans),nrow(humans):1),c(humans$upperPI,rev(humans$lowerPI)),col=colgreenlight,border=F)
+lines(humans$mean~x,col=colgreen,lwd=2)
+dev.off()
+
+
+
+
+
+
+
+
+
+
---
+++ Computational part/SEIRVQ_run.R
@@ -0,0 +1,132 @@
+## Quarantine model 
+## Name: Isty Rysava
+## Date: 08/02/18
+## Code: runs  SEIR.tauleap model to test intervention scenarios (as per "thesis_params.R")
+
+rm(list=ls())
+setwd("~/Dropbox/HongKong")
+
+### Libraries 
+source("R/TauLeap_SEIRfc_vacc_explicit_updated.R")
+source("R/thesis_params.R")
+source("R/thesis_vaccination_params.R")
+
+### Parameters 
+parameters <- vector("list", length=2)
+# vaccweekly5yrs <- 0.5*vaccweekly5yrs # reduce vaccination covergae by 50% 
+vaccweekly5yrs <- vaccweekly5yrs + (0.5*vaccweekly5yrs) # increase vaccination covergae by 50% 
+parameters[[1]] <- c(0, vaccweekly5yrs) 
+parameters[[2]] <- params3 # params1, params2, params3 (different quarantine treatments)
+
+### Initial conditions 
+initials <- c(S=S, V1=V1, V2=V2, V3=V3, E=E, I=I, Qs=0, Qer=0, Qeb=0, Qi=0,
+              Rill=0, Sh=1324935, Eh=0, Ih=0,Rh=0, Vhs=0, Vhe=0) 
+# humpop Google: 1,314,826 in 2015, mine: 1,324,935
+
+end.time <- 52*5
+nsim <- 1000
+allout <- vector("list", length=nsim)
+allcounts <- vector("list", length=nsim)
+
+### Run the model: simulations
+for(sim in 1:nsim){
+  sierTL.out <- SEIR.tauleap(init = initials, pars = parameters,
+                             end.time = end.time, tau=1) # 1/52 weekly steps
+  allout[[sim]] <- as.matrix(sierTL.out$results)
+  allcounts[[sim]] <- as.matrix(sierTL.out$counts)
+  print(sim)
+}
+
+## original
+# saveRDS(allout, "output/out_thesis_scenario1.Rdata") # params 1
+# saveRDS(allcounts, "output/counts_thesis_scenario1.Rdata") # params 1
+# saveRDS(allout, "output/out_thesis_scenario2.Rdata") # params 2
+# saveRDS(allcounts, "output/counts_thesis_scenario2.Rdata") # params 2
+# saveRDS(allout, "output/out_thesis_scenario3.Rdata") # params 3
+# saveRDS(allcounts, "output/counts_thesis_scenario3.Rdata") # params 3
+
+## 50% vaccination
+# saveRDS(allout, "output/out_thesis_scenario1halfvacc.Rdata") # params 1
+# saveRDS(allcounts, "output/counts_thesis_scenario1halfvacc.Rdata") # params 1
+# saveRDS(allout, "output/out_thesis_scenario2halfvacc.Rdata") # params 2
+# saveRDS(allcounts, "output/counts_thesis_scenario2halfvacc.Rdata") # params 2
+# saveRDS(allout, "output/out_thesis_scenario3halfvacc.Rdata") # params 3
+# saveRDS(allcounts, "output/counts_thesis_scenario3halfvacc.Rdata") # params 3
+
+## extra 50% vaccination
+# saveRDS(allout, "output/out_thesis_scenario1extravacc.Rdata") # params 1
+# saveRDS(allcounts, "output/counts_thesis_scenario1extravacc.Rdata") # params 1
+# saveRDS(allout, "output/out_thesis_scenario2extravacc.Rdata") # params 2
+# saveRDS(allcounts, "output/counts_thesis_scenario2extravacc.Rdata") # params 2
+# saveRDS(allout, "output/out_thesis_scenario3extravacc.Rdata") # params 3
+# saveRDS(allcounts, "output/counts_thesis_scenario3extravacc.Rdata") # params 3
+
+### Run the model: testing
+end.time <- 52*5
+sierTL.out <- SEIR.tauleap(init = initials, pars = parameters,
+                           end.time = end.time, tau=1) # 1/52 weekly steps
+out <- sierTL.out$results
+counts <- sierTL.out$counts
+
+out <- as.matrix(out)
+counts <- as.data.frame(counts)
+colnames(counts) <- c("S", "V1", "V2", "V3", "E", "I", "Qs", "Qer", "Qeb", "Qi", "Rill", "Sh", "Eh",
+                   "Ih", "Rh", "Vhs", "Vhe")
+head(counts); tail(counts)
+
+### Plot
+# dog population
+plot(seq(0, end.time, by=1), out[,2], type="l", ylim=c(0,max(out[,2]))) # sus
+lines(seq(0, end.time, by=1), rowSums(out[,3:5]), col="orange") # all vacc
+
+# dog vaccination
+plot(seq(0, end.time, by=1), out[,3],  type="l", col="red", ylim=c(0,max(out[,3]))) # V1
+lines(seq(0, end.time, by=1), out[,4], col="pink") # V2
+lines(seq(0, end.time, by=1), out[,5], col="violet") # V3
+
+# dog quarantine
+plot(seq(0, end.time, by=1), rowSums(out[,8:11]), type="l") # all in qurantine
+lines(seq(0, end.time, by=1), rowSums(counts[,10:11]), col="red") # exp+inf in quarantine that will be removed
+
+# dog infection in quarantine
+# plot(seq(0, end.time, by=1), counts$Qs, type="l", ylim=c(0,max(counts$Qs))) # Qs
+# lines(seq(0, end.time, by=1), counts$Qer, col="green") # Qer
+# lines(seq(0, end.time, by=1), counts$Qeb, col="blue") # Qeb
+# lines(seq(0, end.time, by=1), counts$Qi, col="darkblue") # Qi
+
+plot(seq(0, end.time, by=1), out[,8], type="l", ylim=c(0,max(out[,8]))) # Qs
+lines(seq(0, end.time, by=1), out[,9], col="green") # Qer
+lines(seq(0, end.time, by=1), out[,10], col="blue") # Qeb
+lines(seq(0, end.time, by=1), out[,11], col="darkblue") # Qi
+
+# dog infection in dogs and humans 
+plot(seq(0, end.time, by=1), counts$I, type="l") # I dogs
+lines(seq(0, end.time, by=1), counts$Ih, col="red") # I humans
+
+plot(seq(0, end.time, by=1), out[,7], type="l") # I dogs
+lines(seq(0, end.time, by=1), out[,15], col="red") # I humans
+
+plot(seq(0, end.time, by=1), counts$Rill, type="l", ylim=c(0,max(counts$Rill))) # dead dogs
+lines(seq(0, end.time, by=1), counts$Rh, col="red") # dead humans
+
+plot(seq(0, end.time, by=1), counts$Rill, type="l") # Rabid dead dogs
+lines(seq(0, end.time, by=1), counts$I, col="red") # Rabid alive dogs
+sum(counts$I)
+sum(counts$Rill)
+
+# plot(seq(0, end.time, by=1), out[,12], type="l") # dead dogs
+# lines(seq(0, end.time, by=1), out[,16], col="red") # dead humans: this is a large number, but probably beacuse of so many infected dogs
+
+# PEP uptake
+plot(seq(0, end.time, by=1), counts$Vhs, type="l") # Vhs
+lines(seq(0, end.time, by=1), counts$Vhe, col="blue") # Vhe
+lines(seq(0, end.time, by=1), counts$Eh, col="red") # Eh (not given PEP)
+
+sum(counts$Eh)
+sum(counts$Ih)
+sum(counts$Rh)
+
+# # Thesis word count
+# 736+3799+2767+1159+6040+3645+1904+1262+2030
+
+
