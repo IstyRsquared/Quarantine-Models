ab6fdaafb437acbd8923293e6ae9c4e173216fcd
IstyRsquared <istyrysava@gmail.com>
2023-11-21 17:52:51 -0500

    clean dates

--- terminal_runs/SEIRVQ_run_4.R
+++ terminal_runs/SEIRVQ_run_4.R
@@ -1,6 +1,6 @@
 ## Quarantine model 
 ## Name: Isty Rysava
-## Date: 08/02/18
+## Date: 08/02/21
 ## Code: runs  SEIR.tauleap model to test intervention scenarios 
 
 rm(list=ls())
@@ -8,8 +8,8 @@ setwd("~/Documents/Rabies_Warwick/Quarantine-models")
 
 ### Libraries 
 source("R/TauLeap_SEIRfc_vacc_explicit_updated.R")
-# source("R/quarantine_params.R")
-source("R/incursion_params.R")
+source("R/quarantine_params.R")
+# source("R/incursion_params.R")
 
 ### Parameters and initial conditions 
 ## Simulation
@@ -69,7 +69,7 @@ for(idx in 1:nrow(params_grid)){
     deadH[,sim] <- pops.temp[,"Rh"]
     pop[,sim] <- res.temp[,"S"] + res.temp[,"V1"] + res.temp[,"V2"] + res.temp[,"V3"] + 
       res.temp[,"E"] + res.temp[,"I"] + res.temp[,"Qs"] + res.temp[,"Qer"] + res.temp[,"Qeb"] + res.temp[,"Qi"]
-    rm( sierTL.out)
+    rm(sierTL.out)
   }
   
   ### extract info & save
@@ -78,7 +78,7 @@ for(idx in 1:nrow(params_grid)){
   print(idx)
   # print(params_grid[idx,])
   vc.t <- 1-exp(-params_grid$vc[idx]*53)
-  saveRDS(allout, paste0("output/incs/MS_sim_runs_R0", parameters["R0"], 
+  saveRDS(allout, paste0("output/incursions/MS_sim_runs_R0", parameters["R0"], 
                          "_sqc", params_grid$sqc[idx],
                          "_vc", vc.t, ".Rdata")) 
 }
